bc98a67f2cef72d0ce995fcc6bcc166833ce9789
rayung999 <kimrawun@naver.com>
2022-10-23 23:02:54 +0900

    '.'

--- app/src/main/java/com/goteatfproject/appgot/App.java
+++ app/src/main/java/com/goteatfproject/appgot/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
@@ -15,11 +14,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("비트캠프 프로젝트!");
     SpringApplication.run(App.class, args);
-  }
-
-  @GetMapping("/")
-  public String welcome() {
-    return "welcome";
   }
 
 }
