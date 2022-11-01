c499b3a3901f3044b37429c8438e643ce5c688a7
seulgi90 <seulgi6380@gmail.com>
2022-11-01 11:17:12 +0900

    mypage

--- app/src/main/java/com/goteatfproject/appgot/web/LoginController.java
+++ app/src/main/java/com/goteatfproject/appgot/web/LoginController.java
@@ -1,7 +1,5 @@
 package com.goteatfproject.appgot.web;
 
-import com.goteatfproject.appgot.service.MemberService;
-import com.goteatfproject.appgot.vo.User;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.goteatfproject.appgot.service.MemberService;
+import com.goteatfproject.appgot.vo.User;
 
 @Controller
 @RequestMapping("/auth")
@@ -23,38 +23,38 @@ public class LoginController {
   }
 
   @GetMapping("/login")
-    public String login(@CookieValue(name="id", defaultValue = "") String id, Model model) throws Exception {
-      model.addAttribute("id", id);
-      return "auth/login"; // TODO login 다시 복구
+  public String login(@CookieValue(name="id", defaultValue = "") String id, Model model) throws Exception {
+    model.addAttribute("id", id);
+    return "auth/login"; // TODO login 다시 복구
   }
 
 
   @PostMapping("/login")
-    public String login(
-        String id,
-        String password,
-        String saveEmail,
-        String toUrl, // TODO url2
-        HttpServletResponse response,
-        HttpSession session) throws Exception {
+  public String login(
+      String id,
+      String password,
+      String saveEmail,
+      String toUrl, // TODO url2
+      HttpServletResponse response,
+      HttpSession session) throws Exception {
 
-      User user = memberService.get(id, password);
+    User user = memberService.get(id, password);
 
     System.out.println("password = " + password);
     System.out.println("userpassword = " + user.getPassword());
 
-      if (user != null) {
-        session.setAttribute("loginMember", user);
-      }
+    if (user != null) {
+      session.setAttribute("loginMember", user);
+    }
 
-      Cookie cookie = new Cookie("id", id);
-      if (saveEmail == null) {
-        cookie.setMaxAge(0);
-      } else {
-        cookie.setMaxAge(60 * 60 * 24 * 7);
-      }
+    Cookie cookie = new Cookie("id", id);
+    if (saveEmail == null) {
+      cookie.setMaxAge(0);
+    } else {
+      cookie.setMaxAge(60 * 60 * 24 * 7);
+    }
 
-      response.addCookie(cookie);
+    response.addCookie(cookie);
 
     return "index";
   }
