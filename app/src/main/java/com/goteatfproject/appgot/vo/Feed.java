9e082e36ed82d662fa50ca6a6522014dbf54f8c1
rayung9999 <kimrawun@naver.com>
2022-11-02 17:47:15 +0900

    피드 추가

---
+++ app/src/main/java/com/goteatfproject/appgot/vo/Feed.java
@@ -0,0 +1,16 @@
+package com.goteatfproject.appgot.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Getter @Setter @ToString
+public class Feed {
+
+  private int no;
+  private String title;
+  private String content;
+
+}
